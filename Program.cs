574ce00f58e0539e581f8a3639ea5f403fc312d6
DrSamNemo <dr.samuel.nemo@gmail.com>
2012-08-31 11:59:12 -0400

    YabaDabbaDooooooo

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.IO;
+
+namespace OutlookTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Outlook.Application app = null;
+
+            if (Process.GetProcessesByName("OUTLOOK").Count() > 0)
+            {
+                app = Marshal.GetActiveObject("Outlook.Application") as Outlook.Application;
+                Console.WriteLine("Got the active outlook object!");
+            }
+            else
+            {
+                app = new Outlook.Application();
+                Outlook.NameSpace ns = app.GetNamespace("MAPI");
+                ns.Logon("", "", Missing.Value, Missing.Value);
+                ns = null;
+                Console.WriteLine("Created a new session!");
+            }
+
+            if (app != null)
+            {
+                Outlook.NameSpace ns = app.GetNamespace("MAPI");
+                foreach (Outlook.MAPIFolder folder in ns.Folders)
+                {
+                    Console.WriteLine("{0}", folder.Name);
+                    foreach (Outlook.MAPIFolder innerFolder in folder.Folders)
+                    {
+                        Console.WriteLine("++{0}", innerFolder.Name);
+                        if (innerFolder.Name == "Inbox")
+                        {
+                            Outlook.Items items = innerFolder.Items;
+                            foreach (Outlook.MailItem item in items)
+                            {
+                                Console.WriteLine("++--{0}", item.Subject);
+                                foreach (Outlook.Attachment att in item.Attachments)
+                                {
+                                    Console.WriteLine("++--..{0}: {1}", att.FileName, att.PathName);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                Console.In.Read();
+
+                ns.Logoff();
+            }
+        }
+    }
+}
